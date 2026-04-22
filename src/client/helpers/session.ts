467a6f6da324bb4c541cf25f5d493efb77f4479a
destiny <desto4q@gmail.com>
2026-04-23 00:09:20 +0100

    tsconfig

--- src/client/helpers/session.ts
+++ src/client/helpers/session.ts
@@ -1,28 +1,24 @@
-import { parse } from "cookie";
-import { isTokenExpired } from "pocketbase";
-import { unstable_getContext } from "waku/server";
-import { ssr_pb } from "../pb";
+// import { unstable_getContext } from "waku/server";
 
-export const get_initial_user = () => {
-  const pb = ssr_pb();
-  try {
-    const ctx = unstable_getContext();
-    const cookieHeader = ctx.req.headers.get("cookie") ?? "";
-    const cookies = parse(cookieHeader);
-    const match = cookies["pb_auth"];
-    if (!match) return null;
-    const token = JSON.parse(decodeURIComponent(match))?.token;
-    if (!token || isTokenExpired(token)) return null;
-    pb.authStore.loadFromCookie(`pb_auth=${match}`);
-    const model = pb.authStore.record;
-    // console.log("note expired", model);
-    if (!model?.id) return null;
-    return {
-      id: model.id as string,
-      username: (model.username ?? model.name ?? "User") as string,
-      email: (model.email ?? "") as string,
-    };
-  } catch {
-    return null;
-  }
-};
+// export const get_initial_user = () => {
+//   const pb = ssr_pb();
+//   try {
+//     const cookieHeader = ctx.req.headers.get("cookie") ?? "";
+//     const cookies = parse(cookieHeader);
+//     const match = cookies["pb_auth"];
+//     if (!match) return null;
+//     const token = JSON.parse(decodeURIComponent(match))?.token;
+//     if (!token || isTokenExpired(token)) return null;
+//     pb.authStore.loadFromCookie(`pb_auth=${match}`);
+//     const model = pb.authStore.record;
+//     // console.log("note expired", model);
+//     if (!model?.id) return null;
+//     return {
+//       id: model.id as string,
+//       username: (model.username ?? model.name ?? "User") as string,
+//       email: (model.email ?? "") as string,
+//     };
+//   } catch {
+//     return null;
+//   }
+// };
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,9 @@
 import { defineConfig } from "vite";
 import { devtools } from "@tanstack/devtools-vite";
-
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
-
 import viteReact from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import { nitro } from "nitro/vite";
-import rsc from "@vitejs/plugin-rsc";
 const config = defineConfig({
   resolve: { tsconfigPaths: true },
   plugins: [
